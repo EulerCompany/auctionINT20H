1dcf6c9c8854d5d29e79430b026975c487c182da
Maksym Tsybin <mtsybin@magnite.com>
2024-02-05 20:32:15 +0200

    tailwind + base routing setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import { Layout } from './components/Layout.jsx'
+import { Routes, Route } from 'react-router-dom'
+import { ListingPage } from './pages/ListingPage.jsx'
+import { LoginPage } from './pages/LoginPage.jsx'
+import { RegistrationPage } from './pages/RegistrationPage.jsx'
 
 function App() {
   return (
-    <div className="App"></div>
-  );
-}
+  <Layout>
+    <Routes>
+      <Route path='/' element={ <ListingPage /> } />
+      <Route path='login' element={ <LoginPage /> } />
+      <Route path='registration' element={ <RegistrationPage /> } />
+    </Routes>
+  </Layout>
+
+)}
 
 export default App;
