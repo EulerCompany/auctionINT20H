e21f6254008967f546cb598754e2678f24dd24f0
Vladyslav <chaikovskyivv@gmail.com>
2024-02-10 16:42:21 +0200

    Listing page initial implementation

--- frontend/src/pages/ListingPage.jsx
+++ frontend/src/pages/ListingPage.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
+import { Space, Table, Tag } from 'antd';
+import type { TableProps } from 'antd';
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchAllAuctions } from '../redux/features/auction/auctionSlice'
 
-export const ListingPage = () => {
-  return (
-    <div>ListingPage</div>
-  )
+const columns: TableProps<DataType>['columns'] = [
+  {
+    title: 'Title',
+    dataIndex: ['title' ],
+    render: (title, id) => <a href={"/auction/"}  >{title}</a>,
+  },
+];
+
+export const ListingPage: React.FC = () => {
+    const loading = useSelector((state) => {
+        console.log(state);
+        return state.auction.loading
+    })
+    const auctions = useSelector((state) => {
+        return state.auction.auctions
+    })
+    const pageSize = useSelector((state) => {
+        return state.auction.pageSize
+    })
+    
+    const totalPages = useSelector((state) => {
+        return state.auction.totalPages
+    })
+    
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchAllAuctions());
+    }, [dispatch])
+    return (
+        <Table
+            loading={loading}
+            columns={columns}
+            dataSource={auctions}
+            pagination={{
+                pageSize: pageSize,
+                total: totalPages,
+            }}
+        />
+    )
 }
--- frontend/src/redux/features/auction/auctionSlice.js
+++ frontend/src/redux/features/auction/auctionSlice.js
@@ -2,15 +2,8 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from '../../../utils/axios'
 
-const initialState = {
-    user: null,
-    token: null,
-    isLoading: false,
-    status: null,
-}
-
 export const createAuction = createAsyncThunk(
-    'auction/createAuction', 
+    'auctions/createAuction', 
     async ({username, password}) => {
         try {
             const { data } = await axios.post('/auction/create', {
@@ -19,45 +12,66 @@ export const createAuction = createAsyncThunk(
             })
             // TODO: remove log
             console.log(data)
-            if(data.token) {
-                window.localStorage.setItem('token', data.token)
-            }
-
             return data
             
         } catch (error) {
             console.log(error)
         }
+}, {
+    pending: (state) => {},
+    rejected: (state, action) => {},
+    fulfilled: (state, action) => {},
+
+})
+
+export const fetchAllAuctions = createAsyncThunk(
+    'auctions/fetchAllAuctions',
+    async () => {
+        try {
+            const { data } = await axios.get('/auction/active')
+            // TODO: remove log
+            console.log(data)
+            return data
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+, { // why this isn't working?
+    pending: (state) => {
+    },
+    rejected: (state, action) => {
+    },
+    fulfilled: (state, action) => {
+    },
 })
 
 export const auctionSlice = createSlice({
-    name: 'auction',
-    initialState,
+    name: 'auctions',
+    initialState: {
+        loading: false,
+        pageSize: 10,
+        totalPages: 0,
+        auctions: []
+    },
     reducers: {
-        logout: (state) => {
-            state.user = null
-            state.token = null
-            state.isLoading = false
-            state.status = null
-        }
     },
     extraReducers: builder => {
-        builder.addCase(createAuction.pending, (state) => {
-            state.isLoading = true
-            state.status = null
+        builder.addCase(fetchAllAuctions.pending, (state) => { 
+            console.log("pending.... at fetchall")
+            state.loading = true
         })
-        builder.addCase(createAuction.fulfilled, (state, action) => {
-            state.isLoading = false
-            state.status = action.payload.message
-            state.user = action.payload.user
-            state.token = action.payload.token
+        builder.addCase(fetchAllAuctions.fulfilled, (state, action) => {
+            console.log("fulfilled")
+            state.auctions = action.payload
+            state.totalPages = action.payload
+            state.loading = false
         })
-        builder.addCase(createAuction.rejected, (state, action) => {
-            state.status = action.payload.message
-            state.isLoading = false
+        builder.addCase(fetchAllAuctions.rejected, (state, action) => {
+            console.log("rejected")
+            state.loading = false
         })
-    },
-})
+}})
 
 
 // Why do we need this?
